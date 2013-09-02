7afdb5192f4bb699954612ee63e0067518329b43
David van Dijk <david.van.dijk@dop.nu>
2013-09-02 15:17:01 +0200

    Styling carousel

--- themes/dimpact/js/dimpact.js
+++ themes/dimpact/js/dimpact.js
@@ -1,5 +1,27 @@
 (function($) {
   $(window).load(function () {
 
+    $(".carousel-93 .views-rows").carouFredSel({
+      items: 1,
+      scroll: {
+        fx: "directscroll",
+        duration: 1000,
+        pauseOnHover: true
+      },
+      auto: {
+        timeoutDuration: 10000,
+        progress: ".progress"
+      },
+      pagination: ".pager",
+      next: {
+        button: ".btn_next",
+        key: "right"
+      },
+      prev: {
+        button: ".btn_prev",
+        key: "left"
+      }
+    });
+
   });
 })(window.jQuery);
--- themes/dimpact/templates/views/views-view-unformatted.tpl.php
+++ themes/dimpact/templates/views/views-view-unformatted.tpl.php
@@ -6,9 +6,12 @@
  * @ingroup views_templates
  */
 ?>
+
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <?php print $row; ?>
+  <div class="views-row">
+    <?php print $row; ?>
+  </div>
 <?php endforeach; ?>
