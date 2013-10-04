a13e8b7debb14edd55b0e0cbbb5ec450b0ab0b21
Maurits Lawende <mauritsl@243897.no-reply.drupal.org>
2013-10-04 11:31:15 +0200

    Issue #64: Add ckeditor_customtags module

---
+++ modules/ckeditor_customtags/plugins/deletion/deletion.js
@@ -0,0 +1,29 @@
+/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+ 
+CKEDITOR.plugins.add('ckeditor_deletionbutton', {
+  init: function( editor ) {
+    editor.addCommand('ckeditor_deletionbutton', {
+      exec: function(e) {
+        var mySelection = editor.getSelection();
+        if (CKEDITOR.env.ie) {
+            mySelection.unlock(true);
+            selectedText = mySelection.getNative().createRange().text;
+        } else {
+            selectedText = mySelection.getNative();
+        }
+        
+        var element = '<del>' + selectedText + '</del>';
+        editor.insertHtml(element);
+      }
+    });
+    editor.ui.addButton('ckeditor_deletionbutton', {
+      label: 'Deletion',
+      command: 'ckeditor_deletionbutton',
+      icon: '/sites/all/modules/custom/ckeditor_customtags/plugins/deletion/deletion.png'
+    }); 
+   
+  }
+});
