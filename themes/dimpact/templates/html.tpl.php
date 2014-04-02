48b4826647df8aa7f66540e0fac61bab2499884d
MarcKwee <dop@aenema.local>
2014-04-02 10:39:34 +0200

    Ticket 20 CSS uploader aangepast op basis van Jeroen

--- themes/dimpact/templates/html.tpl.php
+++ themes/dimpact/templates/html.tpl.php
@@ -10,19 +10,18 @@
     <?php 
       module_load_include('admin.inc', 'dimpact_custom_css', 'dimpact_custom_css');
       global $_domain;
+      global $base_url;
+
       if (domain_conf_variable_get($_domain['domain_id'], 'css_killswitch')) {
         foreach (dimpact_custom_css_files() as $key => $title) {
 
           $file = file_load(domain_conf_variable_get($_domain['domain_id'], $key));
 
-          if ($key == 'custom_css_screen' || $key == 'custom_css_print') {
-            print '<link rel="stylesheet" href="' . conf_path() . '/files/custom_css/' . $file->filename . '" media="screen" />';
+          if ($key == 'custom_css_screen') {
+            print '<link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '" media="screen" />';
           }
-          if ($key == 'custom_css_lteie9') {
-            print '<!--[if lte IE 9]><link rel="stylesheet" href="' . conf_path() . '/files/custom_css/' . $file->filename . '" media="all" /><![endif]-->';
-          }
-          if ($key == 'custom_css_ltie9') {
-            print '<!--[if lt IE 9]><link rel="stylesheet" href="' . conf_path() . '/files/custom_css/' . $file->filename . '" media="all" /><![endif]-->';
+          if ($key == 'custom_css_print') {
+            print '<link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '" media="print" />';
           }
         }
       }
@@ -41,6 +40,18 @@
       <script src="/<?php print $path; ?>/js/libraries/html5shiv/src/html5shiv.js"></script>
       <script src="/<?php print $path; ?>/js/libraries/respond.min.js"></script>
     <![endif]-->
+    <?php if (domain_conf_variable_get($_domain['domain_id'], 'css_killswitch')) {
+        foreach (dimpact_custom_css_files() as $key => $title) {
+          $file = file_load(domain_conf_variable_get($_domain['domain_id'], $key));
+                    if ($key == 'custom_css_lteie9') {
+            print '<!--[if lte IE 9]><link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '" media="all" /><![endif]-->';
+          }
+          if ($key == 'custom_css_ltie9') {
+            print '<!--[if lt IE 9]><link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '" media="all" /><![endif]-->';
+          }
+        }
+      } 
+    ?>
   </head>
   <body<?php print $body_attributes;?>>
     <a href="#main" class="visuallyhidden focusable skip-link"><?php print t('Skip to main content'); ?></a>
