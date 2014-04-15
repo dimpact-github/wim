d72095bd74a3e973f4102900c6ba4d4966dd5fe0
Marc Kwee <marc.kwee@dop.nu>
2014-04-15 12:26:04 +0200

    Ticket nr 22 - Content reminder module v1.0

--- modules/content_reminder/content_reminder.tpl.php
+++ modules/content_reminder/content_reminder.tpl.php
@@ -5,15 +5,16 @@
   </head>
 
   <body style="background-color: #FFFFFF">
-    <p>De volgende nodes zijn niet geupdate</p>
-    
-    <?php
+    <?php 
+      print '<p>';
+        print variable_get('content_reminder_' . $mailtype . '_text', 0);
+      print '</p>';
         print '<ul>';
         foreach($nodes as $node) {
           if ($node->type) {
-              print '<li><a href="/node/' . $node->nid . '">';
+              print '<li>' . '<strong>' . $node->type . '</strong>: ' . '<a href="/node/' . $node->nid . '">';
                 print $node->title;
-              print '</li>';
+              print '</a> ' . ($node->status == 1 ? 'Published' : 'Unpublished') . '</li>';
           }
         }
         print '</ul>';
