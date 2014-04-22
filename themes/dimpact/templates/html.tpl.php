0e4869b8f04f7adb4dd09e2f527599c468b20736
Maurits Lawende <mauritsl@243897.no-reply.drupal.org>
2014-04-22 21:16:43 +0200

    Issue #45: Set a cache key in custom css module. This string is added to the URL to force renewal of browser and Varnish cache.
    Conflicts:
    	themes/dimpact/templates/html.tpl.php

--- themes/dimpact/templates/html.tpl.php
+++ themes/dimpact/templates/html.tpl.php
@@ -3,10 +3,28 @@
   <head>
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
-    <link rel="dns-prefetch" href="//themes.googleusercontent.com">
-    <link rel="dns-prefetch" href="//fonts.googleapis.com">
     <script src="/<?php print $path; ?>/js/modernizr.js"></script>
     <?php print $styles; ?>
+    <?php
+      module_load_include('admin.inc', 'dimpact_custom_css', 'dimpact_custom_css');
+      global $_domain;
+      global $base_url;
+
+      if (domain_conf_variable_get($_domain['domain_id'], 'css_killswitch')) {
+        foreach (dimpact_custom_css_files() as $key => $title) {
+
+          $file = file_load(domain_conf_variable_get($_domain['domain_id'], $key));
+          $cache_key = variable_get('dimpact_custom_css_cache_key', '');
+
+          if ($key == 'custom_css_screen') {
+            print '<link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '?' . $cache_key . '" media="screen" />';
+          }
+          if ($key == 'custom_css_print') {
+            print '<link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '?' . $cache_key . '" media="print" />';
+          }
+        }
+      }
+    ?>
     <?php print $scripts; ?>
 
     <?php if (variable_get('bespoke_readspeaker_status') == 1): ?>
@@ -21,6 +39,18 @@
       <script src="/<?php print $path; ?>/js/libraries/html5shiv/src/html5shiv.js"></script>
       <script src="/<?php print $path; ?>/js/libraries/respond.min.js"></script>
     <![endif]-->
+    <?php if (domain_conf_variable_get($_domain['domain_id'], 'css_killswitch')) {
+        foreach (dimpact_custom_css_files() as $key => $title) {
+          $file = file_load(domain_conf_variable_get($_domain['domain_id'], $key));
+                    if ($key == 'custom_css_lteie9') {
+            print '<!--[if lte IE 9]><link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '?' . $cache_key . '" media="all" /><![endif]-->';
+          }
+          if ($key == 'custom_css_ltie9') {
+            print '<!--[if lt IE 9]><link rel="stylesheet" href="' . $base_url . '/' . conf_path() . '/files/custom_css/' . $file->filename . '?' . $cache_key . '" media="all" /><![endif]-->';
+          }
+        }
+      }
+    ?>
   </head>
   <body<?php print $body_attributes;?>>
     <a href="#main" class="visuallyhidden focusable skip-link"><?php print t('Skip to main content'); ?></a>
