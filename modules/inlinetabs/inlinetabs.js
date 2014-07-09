93f1fe9539d5ef733f9f03c3a831774d3e1df96a
Rolf Meijer <rolf@karu.nl>
2014-07-09 11:51:29 +0200

    Issue #212: actieve tab weergeven
    * tab-lijsten krijgen role="tablist"
    * tabs krijgen role="tab"
    * inhoud krijgt role="tabpanel"
    * tabs krijgen aria-selected="false"
    * actieve tab krijgt aria-selected="true"

--- modules/inlinetabs/inlinetabs.js
+++ modules/inlinetabs/inlinetabs.js
@@ -3,15 +3,17 @@
     attach: function(context) {
       $('.inlinetabs').each(function() {
         var container = this;
-        
+
         $('> ul a', this).click(function() {
           $('> div', container).hide();
           $('> ul li, > ul a', container).removeClass('active');
+          $('> ul a', container).attr('aria-selected', 'false');
           $($(this).attr('href')).show();
           $(this).addClass('active').parent().addClass('active');
+          $(this).addClass('active').attr('aria-selected', 'true');
           return false;
         });
-        
+
         $('> ul a:first', this).click();
       });
     }
