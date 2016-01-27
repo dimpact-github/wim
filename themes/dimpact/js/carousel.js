e42df884d872a3a051e617347c571acd62bd59b7
Eric Alvares <eric.alvares@dop.nu>
2016-01-27 13:42:31 +0100

    Revert "Revert "Ticket #97: Fix play/pause button for carousels (focus).""
    This reverts commit ecfcfb4658f693e3b7ff919564f5234d64487f53.

--- themes/dimpact/js/carousel.js
+++ themes/dimpact/js/carousel.js
@@ -1,16 +1,14 @@
 (function($) {
-// Start carousels.
-Drupal.behaviors.dimpact_carousel = {
-  attach: function(context, settings) {
-    // Carousels
-    if ($(".carousel .views-rows", context).length) {
+
+  Drupal.behaviors.dimpact_carousel = {
+    attach: function(context, settings) {
+
+      if ($(".carousel .views-rows", context).length) {
         // Create Pagination elements via javascript because of reasons.
         var next = '<button class="btn_next" title="' + Drupal.t('Next') + '"><span class="icon-chevron-sign-right"><span class="element-invisible">' + Drupal.t('Next') + '</span></span></a>';
         $(".carousel .views-rows", context).after(next);
-
-        var play_pauze = '<button class="btn_play" title="' + Drupal.t('Play / Pause carousel') + '"><span class="icon-"><span class="element-invisible">' + Drupal.t('Play/Pause') + '</span></span></a>';
-        $(".carousel .views-rows", context).after(play_pauze);
-
+        var play_pause = '<button class="btn_play" title="' + Drupal.t('Play / Pause carousel') + '"><span class="icon-pause"><span class="element-invisible">' + Drupal.t('Play/Pause') + '</span></span></a>';
+        $(".carousel .views-rows", context).after(play_pause);
         var previous = '<button class="btn_prev" title="' + Drupal.t('Previous') + '"><span class="icon-chevron-sign-left"><span class="element-invisible">' + Drupal.t('Previous') + '</span></span></a>';
         $(".carousel .views-rows", context).after(previous);
 
@@ -18,41 +16,45 @@ Drupal.behaviors.dimpact_carousel = {
 
         // Instantiate carousels.
         $(".carousel .views-rows", context).carouFredSel({
-            items: 1,
-            responsive: true,
-            scroll: {
-                fx: "directscroll",
-                duration: 500,
-                // pauseOnHover: 'resume'
+          items: 1,
+          responsive: true,
+          scroll: {
+            fx: "directscroll",
+            duration: 500,
+            // pauseOnHover: 'resume'
+          },
+          auto: {
+            // button makes play/pause functionality possible. pauseonhover must be off, else
+            // it overides the button everytime you hover over other parts of the carousel.
+            button: function() {
+              return $(this).parents('.carousel').find('.btn_play');
             },
-            auto: {
-                // button makes play/pause functionality possible. pauseonhover must be off, else
-                // it overides the button everytime you hover over other parts of the carousel.
-                button: function() {
-                    return $(this).parents('.carousel').find('.btn_play');
-                },
-                timeoutDuration: 10000,
-                progress: {
-                    bar: function() {
-                        return $(this).parents('.carousel').find('.progress');
-                    }
+            timeoutDuration: 10000,
+              progress: {
+                bar: function() {
+                  return $(this).parents('.carousel').find('.progress');
                 }
+              }
             },
             next: {
-                button: function() {
-                    return $(this).parents('.carousel').find('.btn_next');
-                },
-                key: "right"
+              button: function() {
+                return $(this).parents('.carousel').find('.btn_next');
+              },
+              key: "right"
             },
             prev: {
-                button: function() {
-                    return $(this).parents('.carousel').find('.btn_prev');
-                },
-                key: "left"
+              button: function() {
+                return $(this).parents('.carousel').find('.btn_prev');
+              },
+              key: "left"
             }
         });
-        }
+
+        $('.carousel .btn_play').click(function() {
+          $(this).find('> span').toggleClass('icon-pause icon-play');
+        });
+      }
     }
-};
+  };
 
 })(jQuery);
